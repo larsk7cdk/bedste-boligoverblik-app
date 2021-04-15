fc2114556ef8e65d534f6331645491256d857139
Lars <lal@jyskebank.dk>
2021-04-15 21:53:45 +0200

    fixed prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://bedste-boligoverblik-api.azurewebsites.net',
+  apiBaseUrl: 'https://bedste-boligoverblik-api.azurewebsites.net/api',
 };
