dfdecb8dcced52c1f2d929c65e97ad4b1399c97d
Lars Larsen <larsk7cdk@users.noreply.github.com>
2021-05-09 18:22:28 +0200

    Update street-view.component.ts

--- src/app/features/shared/components/street-view/street-view.component.ts
+++ src/app/features/shared/components/street-view/street-view.component.ts
@@ -21,6 +21,6 @@ export class StreetViewComponent implements OnInit {
   latitude: number;
 
   ngOnInit(): void {
-    this.src = `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${this.longitude},${this.latitude}&fov=80&heading=80&pitch=0&key=AIzaSyBangEFVsTXt68GZQop7c7y1PUR_elunX4`;
+    this.src = `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${this.longitude},${this.latitude}&fov=80&heading=80&pitch=0&key=`;
   }
 }
